1e322d30348548b6416de7e402520ca302f79a07
rochana2010 <vshajwggwhwh@gmail.com>
2025-06-05 17:41:12 +0530

    Update Song.js

--- plugins/Song.js
+++ plugins/Song.js
@@ -46,7 +46,7 @@ async(conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sende
             document: { url: audio.download },
             mimetype: 'audio/mp3',
             fileName: `${audio.title}.mp3`,
-            caption: `your name`
+            caption: `・‥…━━━━━━━☆𝙍𝙊𝘾𝙃𝘼𝙉𝘼☆━━━━━━━…‥・`
         });
 
         await conn.sendMessage(from, {
