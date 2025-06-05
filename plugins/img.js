59a49c4c34435c4c9530af5700c7b5181badcb72
rochana2010 <vshajwggwhwh@gmail.com>
2025-06-05 16:58:40 +0530

    Update img.js

--- plugins/img.js
+++ plugins/img.js
@@ -37,7 +37,7 @@ cmd({
           'image': {
             'url': _0x58b5b7.images_url
           },
-          'caption': "*© 𝖦𝖤𝖭𝖤𝖱𝖠𝖳𝖤𝖣 𝖡𝖸 your Botname*" 
+          'caption': "*© 𝖦𝖤𝖭𝖤𝖱𝖠𝖳𝖤𝖣 𝖡𝖸 ROCHANA-MD*" 
         }, {
           'quoted': _0x59fdb9
         });
@@ -93,9 +93,9 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                 caption: `
 *💗 Image ${i + 1} from your search! 💗*
 
- *©  𝖦𝖤𝖭𝖤𝖱𝖠𝖳𝖤𝖣 𝖡𝖸 your Botname 👾*
+ *©  𝖦𝖤𝖭𝖤𝖱𝖠𝖳𝖤𝖣 𝖡𝖸 ROCHANA-MD 👾*
 
-> 🥷🏽 Your Botname 🥷🏽`
+> 🥷🏽  ROCHANA-MD 🥷🏽`
 }, { quoted: mek });
 }
 
